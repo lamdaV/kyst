6a9ae0f35af243fe39385873c52aed415f594c71
David Lam <dav_lam@ymail.com>
2017-12-06 11:35:28 -0500

    updated example to find N kitties

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 const KystClient = require("./KystClient");
 
-client = new KystClient();
-
 const CONVERSION_FACTOR = 10 ** -18
-client.getMedianPriceUSD()
-  .then((medianPriceUSD) => client.convertUSD2ETH(medianPriceUSD)
-    .then((medianPriceETH) => client.getAuctions(0, 10, "sale", "open")
-      .then((response) => {
-        return response.auctions
-          .map((auction) => {
-            auction.eth = auction.current_price * CONVERSION_FACTOR;
-            return auction;
+
+const findNKitties = (n) => {
+  const client = new KystClient();
+
+  const find = (found, offset, limit) => {
+    if (found >= n) {
+      return;
+    }
+
+    client.getMedianPriceUSD()
+      .then((medianPriceUSD) => client.convertUSD2ETH(medianPriceUSD)
+        .then((medianPriceETH) => client.getAuctions(offset, limit, "sale", "open")
+          .then((response) => {
+            return response.auctions
+              .map((auction) => {
+                auction.eth = auction.current_price * CONVERSION_FACTOR;
+                return auction;
+              })
+              .filter((auction) => auction.eth <= medianPriceETH)
+              .map((buyAuction) => `https://www.cryptokitties.co/kitty/${buyAuction.kitty.id}`)
           })
-          .filter((auction) => auction.eth <= medianPriceETH)
-          .map((buyAuction) => buyAuction.kitty.id)
-          .map((kittyId) => `https://www.cryptokitties.co/kitty/${kittyId}`)
-      })
-      .then((matchedUrls) => console.log(matchedUrls))));
+          .then((matchedUrls) => {
+            console.log(matchedUrls);
+            const discover = matchedUrls.length;
+            find(found + discover, offset + limit, limit);
+          })));
+  }
+
+  find(0, 0, 10);
+}
+
+findNKitties(3);
